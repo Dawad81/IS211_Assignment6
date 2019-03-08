ae8a4e53630ec73f3c1b9b470b7dd1685d54aef7
Dawad81 <dennis.awad83@spsmail.cuny.edu>
2019-03-07 21:01:33 -0500

    compleated par1 & 2

--- conversions.py
+++ conversions.py
@@ -1,2 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""This module does some tempature conversions."""
+
+
+def convertCelsiusToKelvin(temp_C):
+    kelvin = float(temp_C) + 273.15
+    return round(kelvin, 2)
+
+
+def convertCelsiusToFahrenheit(temp_C):
+    fahrenheit = (float(temp_C) * (1.8)) + 32
+    return round(fahrenheit,2)
+
+
--- tests.py
+++ tests.py
@@ -1,2 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""This module runs test on conversion.py module."""
+
+import conversions
+import unittest
+
+
+class KnownValues (unittest.TestCase):
+    KnownTempsCFK =[[300.00, 572.00, 573.15],
+                    [-10.00, 14.00, 263.15],
+                    [-273.15, -459.67, 0.00],
+                    [390.00, 734.00, 663.15],
+                    [140.00, 284.00, 413.15],
+                    [320.00, 608.00, 593.15],
+                    [-150.00, -238.00, 123.15]]
+    def testConvertCelsiusToKelvin(self):
+        """convertCelsiusToKelvin should give known result with known input"""
+        for degree in self.KnownTempsCFK:
+            answer = conversions.convertCelsiusToKelvin(degree[0])
+            self.assertEqual(degree[2], answer)
+    def testConvertCelsiusToFahrenheit(self):
+        """convertCelsiusToFahrenheit should give known result with known input"""
+        for degree in self.KnownTempsCFK:
+            answer = conversions.convertCelsiusToFahrenheit(degree[0])
+            self.assertEqual(degree[1], answer)
+
+if __name__ == "__main__":
+    unittest.main()
