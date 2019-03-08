b1126acba1f4bd5837bff82ff71d6d5651cc48d4
Dawad81 <dennis.awad83@spsmail.cuny.edu>
2019-03-08 18:59:46 -0500

    Part 3 complete

--- conversions.py
+++ conversions.py
@@ -4,12 +4,37 @@
 
 
 def convertCelsiusToKelvin(temp_C):
+    """This function converts Celsius to Kelvin."""
     kelvin = float(temp_C) + 273.15
     return round(kelvin, 2)
 
 
 def convertCelsiusToFahrenheit(temp_C):
+    """This function converts Celsius to Fahrenheit."""
     fahrenheit = (float(temp_C) * (1.8)) + 32
     return round(fahrenheit,2)
 
+
+def convertFahrenheitToCelsius(temp_F):
+    """This function converts Fahrenheit to Celsius."""
+    celsius = (float (temp_F) - 32) * 5/9
+    return round(celsius, 2)
+
+
+def convertFahrenheitToKelvin(temp_F):
+    """This function converts Fahrenheit to Kelvin."""
+    kelvin = (float(temp_F) + 459.67) * 5/9
+    return round(kelvin, 2)
+
+
+def convertKelvinToFahrenheit(temp_K):
+    """This function converts Kelvin to Fahrenheit."""
+    fahrenheit = (float(temp_K) * 1.8) - 459.67
+    return round(fahrenheit, 2)
+
+
+def convertKelvinToCelsius(temp_K):
+    """This function converts Kelvin to Celsius."""
+    celsius = float(temp_K) - 273.15
+    return round(celsius, 2)
 
--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""This module runs test on conversion.py module."""
+"""This module runs test on the conversion.py module."""
 
 import conversions
 import unittest
@@ -14,16 +14,49 @@ class KnownValues (unittest.TestCase):
                     [140.00, 284.00, 413.15],
                     [320.00, 608.00, 593.15],
                     [-150.00, -238.00, 123.15]]
+
     def testConvertCelsiusToKelvin(self):
         """convertCelsiusToKelvin should give known result with known input"""
         for degree in self.KnownTempsCFK:
             answer = conversions.convertCelsiusToKelvin(degree[0])
             self.assertEqual(degree[2], answer)
+
+
     def testConvertCelsiusToFahrenheit(self):
         """convertCelsiusToFahrenheit should give known result with known input"""
         for degree in self.KnownTempsCFK:
             answer = conversions.convertCelsiusToFahrenheit(degree[0])
             self.assertEqual(degree[1], answer)
+
+
+    def testConvertFahrenheitToCelsius(self):
+        """convertFahrenheitToCelsius should give known result with known input"""
+        for degree in self.KnownTempsCFK:
+            answer = conversions.convertFahrenheitToCelsius(degree[1])
+            self.assertEqual(degree[0], answer)
+
+
+    def testConvertFahrenheitToKelvin(self):
+        """convertFahrenheitToKelvin should give known result with known input"""
+        for degree in self.KnownTempsCFK:
+            answer = conversions.convertFahrenheitToKelvin(degree[1])
+            self.assertEqual(degree[2], answer)
+
+
+    def testConvertKelvinToFahrenheit(self):
+        """convertKelvinToFahrenheit should give known result with known input"""
+        for degree in self.KnownTempsCFK:
+            answer = conversions.convertKelvinToFahrenheit(degree[2])
+            self.assertEqual(degree[1], answer)
+
+
+    def testConvertKelvinToCelsius(self):
+        """convertKelvinToCelsius should give known result with known input"""
+        for degree in self.KnownTempsCFK:
+            answer = conversions.convertKelvinToCelsius(degree[2])
+            self.assertEqual(degree[0], answer)
+
+
 
 if __name__ == "__main__":
     unittest.main()
